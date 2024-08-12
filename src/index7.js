404cd771d12245d8379afcecac5f1584198f7c1c
Hyolang <gywlsdl1227@hanmail.net>
2024-08-13 01:35:53 +0900

    함수 화살표, 콜백함수, 함수표현식 등

---
+++ src/index7.js
@@ -0,0 +1,35 @@
+//const add = function (a, b) {
+// return a + b;
+//};
+
+const add = (a1, b2) => a1 + b2;
+
+console.log(add(10, 20));
+
+//콜백 함수
+const calculate = (a, b, callback) => {
+  let result = a + b;
+  callback(result);
+};
+
+const printResult = (result) => {
+  console.log("결과: " + result);
+};
+const doubleResult = (result) => {
+  console.log("결과에 2를 곱한 값: " + result * 2);
+};
+
+calculate(5, 3, printResult);
+calculate(5, 3, doubleResult);
+
+const testFunc = (callback) => {
+  callback();
+};
+
+testFunc(() => {
+  console.log("콜백 함수 테스트");
+});
+
+testFunc(function () {
+  console.log("콜백 함수 테스트");
+});
