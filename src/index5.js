dbf5547141444405fefb5761204a58b6f4970978
Hyolang <gywlsdl1227@hanmail.net>
2024-08-12 18:22:27 +0900

    함수 호이스팅, 변수 호이스팅 예제

---
+++ src/index5.js
@@ -0,0 +1,15 @@
+// 함수 호이스팅
+
+connectStrings("study", "hoisting");
+
+function connectStrings(str1, str2) {
+  console.log(str1 + str2);
+}
+
+// 변수 호이스팅
+
+// console.log(num1);
+// let num1 = 123;
+
+console.log(num2);
+var num2 = 123;
