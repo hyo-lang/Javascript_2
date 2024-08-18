d6f8d34f3da1fe437cfd41d0e771f02467b1cf55
Hyolang <gywlsdl1227@hanmail.net>
2024-08-18 23:20:23 +0900

    배열 메서드 1

--- src/index11.js
+++ src/index11.js
@@ -33,3 +33,37 @@ console.log(newArray2);
 
 let colors = ["green", "blue", "purple"];
 console.log(colors.at(-1));
+
+console.log(colors.includes("blue"));
+console.log(colors.includes("yellow"));
+
+console.log(colors.includes("blue", 2));
+console.log(colors.includes("blue", 1));
+
+console.log(colors.indexOf("purple"));
+console.log(colors.indexOf("yellow"));
+
+let colors2 = [
+  { id: 1, color2: "green" },
+  { id: 2, color2: "blue" },
+  { id: 3, color2: "purple" },
+  { id: 4, color2: "yellow" },
+];
+console.log(colors2.indexOf("blue", 1));
+
+colors2.findIndex((elm, idx, array) =>
+  console.log(`${idx} 번째 값은 id: ${elm.id}, color: ${elm.color2}`)
+);
+colors2.findIndex((elm, idx, array) => console.log(array));
+
+let idx = colors2.find((elm) => elm.color2 === "purple");
+
+console.log(idx);
+
+let filterArray = colors2.filter((elm, idx, array) => elm.id > 1);
+
+console.log(filterArray);
+
+let sliceArray = colors2.slice(1, 3);
+
+console.log(sliceArray);
