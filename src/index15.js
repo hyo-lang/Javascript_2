508ed4111e595806a9829efa65885ac9caed43f6
hyo rang <gywlsdl1227@hanmail.net>
2024-08-21 19:51:38 +0900

    비동기 처리

--- src/index15.js
+++ src/index15.js
@@ -12,6 +12,37 @@ workA();
 workB();
 workC();
 
+const work = (callBack) => {
 setTimeout(()=>{
     console.log('비동기');
+    callBack();
 }, 3000);
+};
+
+work(()=> {
+    console.log('종료');
+});
+
+const workD = () => {
+    setTimeout(() => {
+        console.log('workD');
+    }, 5000);
+};
+const workE = () => {
+    setTimeout(() => {
+        console.log('workE');
+    }, 3000);
+};
+const workF = () => {
+    setTimeout(() => {
+        console.log('workF');
+    }, 10000);
+};
+const workG = () => {
+        console.log('workG');
+};
+
+workD();
+workE();
+workF();
+workG();
