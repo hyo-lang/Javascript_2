eb34ea78eb120b76f6bba75e01301b84ab91c1df
hyo rang <gywlsdl1227@hanmail.net>
2024-08-25 23:19:18 +0900

    api 비동기

--- src/index18.js
+++ src/index18.js
@@ -1,1 +1,27 @@
-//api
+let response = fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>console.log(res))
+    .catch((err)=>console.log(err));
+console.log(response);
+
+//
+
+const getData = async () => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/users')
+    let data = await response.json();
+    console.log(response);
+};
+
+getData();
+
+//
+
+const getData1 = async () => {
+    try {
+    let response = await fetch('https://jsonplaceholder.typicode.com/users')
+    let data = await response.json();
+    } catch (err) {
+    console.log(response);
+    }
+};
+
+getData1();
